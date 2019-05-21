d5afcca39d590503c43203e92e46baabf2ddaeef
Bo Zhang <bzhang3@mail.greenriver.edu>
2019-05-21 19:17:59 -0400

    Created PremiumMember class.

---
+++ classes/PremiumMember.php
@@ -0,0 +1,36 @@
+<?php
+    class PremiumMember extends Member
+    {
+        private $_inDoorInterests;
+        private $_outDoorInterests;
+
+        function __construct($fName, $lName, $age, $gender, $phone, $inDoorInterests, $outDoorInterests)
+        {
+            // calls the parent class constructor
+            parent::__construct($fName, $lName, $age, $gender, $phone);
+
+            // sets private fields with passed in parameters
+            $this->_inDoorInterests = $inDoorInterests;
+            $this->_outDoorInterests = $outDoorInterests;
+        }
+
+        function setInDoorInterests($inDoorInterests)
+        {
+            $this->_inDoorInterests = $inDoorInterests;
+        }
+
+        function getInDoorInterests()
+        {
+            return $this->_inDoorInterests;
+        }
+
+        function setOutDoorInterests($outDoorInterests)
+        {
+            $this->_outDoorInterests = $outDoorInterests;
+        }
+
+        function getOutDoorInterests()
+        {
+            return $this->_outDoorInterests;
+        }
+    }
