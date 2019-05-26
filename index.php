24c51bb62c119b8d6b0b4f14e6d368789985d4f4
Bo Zhang <bzhang3@mail.greenriver.edu>
2019-05-26 13:29:58 -0400

    Minor changes here and there.

--- index.php
+++ index.php
@@ -16,53 +16,52 @@
     // turn on f3 error reporting
     $f3->set('DEBUG', 3);
 
+    // creates a database object
+    $db = new Database();
+
     // defines a default route
     $f3->route('GET /', function() {
         //echo '<h1>Welcome To My Dating Website!</h1>';
+
+        // resets session, clears everything
+        $_SESSION = array();
+        //print_r($_SESSION);
 
         // displays a view
         $view = new Template();
         echo $view->render('views/home.html');
     });
 
+    // defines an admin route
+    $f3->route('GET /admin', function($f3) {
+        //print_r($_SESSION);
+
+        global $db;
+        $result = $db->getMembers();
+        $f3->set('result', $result);
+
+        // displays a view
+        $view = new Template();
+        echo $view->render('views/admin.html');
+    });
+
     // defines "Personal Information" route
     $f3->route('GET|POST /personal_info_form', function($f3) {
+        //print_r($_SESSION);
 
-// remove all session variables
-//session_unset();
-
-// destroy the session
-//session_destroy();
-
-        // defines an array of gender with available options
         $f3->set('genderOptions', array('Male', 'Female'));
 
         // if the form has been submitted (via POST), validates it
-        if (!empty($_POST)) {
-            // gets all the data from the form
+        if (!empty($_POST['personal-info-submit'])) {
+            // gets all the data from the personal info form
             $fname = $_POST['fname'];
             $lname = $_POST['lname'];
             $age = $_POST['age'];
-            if (!empty($_POST['gender'])) {
-                $gender = $_POST['gender'];
-            } else {
-                $gender = "(Not specified)";
-            }
+            $gender = $_POST['gender'];
             $phone = $_POST['phone'];
-            if (!empty($_POST['premium'])) {
-                $premium = $_POST['premium'];
-            } else {
-                $premium = "No";
-            }
+            $premium = $_POST['premium'];
 
-            echo "fist name: $fname<br>";
-            echo "last name: $lname<br>";
-            echo "age: $age<br>";
-            echo "gender: $gender<br>";
-            echo "phone: $phone<br>";
-            echo "premium: $premium<br>";
-
-            // stores all the data to hive
+            // stores all the data to hive for the purposes of sticky form and data validation
             $f3->set('fname', $fname);
             $f3->set('lname', $lname);
             $f3->set('age', $age);
@@ -70,55 +69,29 @@
             $f3->set('phone', $phone);
             $f3->set('premium', $premium);
 
-            /*
-            // creates the appropriate class object
-            if ($premium == 'yes') {
-                $premiumMember = new PremiumMember($fname, $lname, $age, $gender, $phone);
-                $f3->set('member', $premiumMember);
-            } else {
-                $member = new Member($fname, $lname, $age, $gender, $phone);
-                $f3->set('member', $member);
-            }
-            */
-
             if (validatePersonalInfoForm()) {
+                // stores data in session
+                $_SESSION['fname'] = $fname; // required
+                $_SESSION['lname'] = $lname; // required
+                $_SESSION['age'] = $age; // required
+                $_SESSION['gender'] = $gender; // optional
+                $_SESSION['phone'] = $phone; // required
+                $_SESSION['premium'] = $premium; // optional
+
                 if ($premium == 'yes') {
+                    // creates a premium member object
                     $premiumMember = new PremiumMember($fname, $lname, $age, $gender, $phone);
+
                     $f3->set('membership', $premiumMember);
                     $_SESSION['membership'] = $premiumMember;
                 } else {
+                    // creates a regular member object
                     $regularMember = new Member($fname, $lname, $age, $gender, $phone);
+
                     $f3->set('membership', $regularMember);
                     $_SESSION['membership'] = $regularMember;
                 }
-                /*
-                // writes data to session variables
-                $_SESSION['fname'] = $fname;
-                $_SESSION['lname'] = $lname;
-                $_SESSION['age'] = $age;
-                $_SESSION['gender'] = $gender;
-                $_SESSION['phone'] = $phone;
-                $_SESSION['premium'] = $premium;
-                */
 
-                //$_SESSION['member'] = $premiumMember;
-
-                // redirects to next form: Profile Form
-                //$f3->reroute('/profile_form');
-            //} else if (validatePersonalInfoForm()) {
-                /*
-                // writes data to session variables
-                $_SESSION['fname'] = $fname;
-                $_SESSION['lname'] = $lname;
-                $_SESSION['age'] = $age;
-                $_SESSION['gender'] = $gender;
-                $_SESSION['phone'] = $phone;
-                $_SESSION['premium'] = $premium;
-                */
-
-                //$_SESSION['member'] = $regularMember;
-
-                // redirects to next form: Profile Form
                 $f3->reroute('/profile_form');
             }
         }
@@ -129,6 +102,9 @@
 
     // defines "Profile" route
     $f3->route('GET|POST /profile_form', function($f3) {
+        //print_r($_SESSION);
+
+        // sets a states array in hive (note that the 1st element is empty)
         $f3->set('stateOptions', array('', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
             'Idaho', 'Illinois', 'Indiana', 'Lowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
             'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon',
@@ -137,42 +113,28 @@
 
         $f3->set('seekingGenderOptions', array('Male', 'Female'));
 
-        if (!empty($_POST)) {
+        if (!empty($_POST['profile-submit'])) {
             $email = $_POST['email'];
             $state = $_POST['state'];
             $seeking = $_POST['seeking'];
             $bio = $_POST['bio'];
 
-            echo "email: $email<br>";
-            echo "state: $state<br>";
-            echo "seeking: $seeking<br>";
-            echo "bio: $bio<br>";
-
-            // todo: necessary? what's the point?
             $f3->set('email', $email);
             $f3->set('state', $state);
             $f3->set('seeking', $seeking);
             $f3->set('bio', $bio);
 
             if (validateProfileForm()) {
-                /*
-                $_SESSION['email'] = $email;
-                $_SESSION['state'] = $state;
-                $_SESSION['seeking'] = $seeking;
-                $_SESSION['bio'] = $bio;
-                */
-                $memberShip = $_SESSION['membership'];
+                $_SESSION['email'] = $email; // required
+                $_SESSION['state'] = $state; // optional
+                $_SESSION['seeking'] = $seeking; // optional
+                $_SESSION['bio'] = $bio; // optional
 
+                $memberShip = $_SESSION['membership'];
                 $memberShip->setEmail($email);
-                if (!empty($state)) {
-                    $memberShip->setState($state);
-                }
-                if (!empty($seeking)) {
-                    $memberShip->setSeeking($seeking);
-                }
-                if (!empty($bio)) {
-                    $memberShip->setBio($bio);
-                }
+                $memberShip->setState($state);
+                $memberShip->setSeeking($seeking);
+                $memberShip->setBio($bio);
 
                 if ($memberShip instanceof PremiumMember) {
                     $f3->reroute('/interests_form');
@@ -188,32 +150,37 @@
 
     // defines "Interests" route
     $f3->route('GET|POST /interests_form', function($f3) {
+        //print_r($_SESSION);
+
         $f3->set('indoorOptions', array('tv', 'movies', 'cooking', 'board games', 'puzzles', 'reading', 'playing cards', 'video games'));
         $f3->set('outdoorOptions', array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing'));
 
-        if ((isset($_POST['submit']))) {
-            $indoorInterests = $_POST['indoor'];
-            $outdoorInterests = $_POST['outdoor'];
+        if (!empty($_POST['interests-submit'])) {
+            $indoorInterests = $_POST['indoorInterests'];
+            $outdoorInterests = $_POST['outdoorInterests'];
 
-            //$f3->set('indoorInterests', $indoorInterests);
-            //$f3->set('outdoorInterests', $outdoorInterests);
+            $f3->set('indoorInterests', $indoorInterests);
+            $f3->set('outdoorInterests', $outdoorInterests);
 
             if (validateInterestsForm()) {
                 $premiumMember = $_SESSION['membership'];
 
                 if (!empty($indoorInterests)) {
                     //$_SESSION['indoorInterests'] = implode(', ', $indoorInterests);
+                    $_SESSION['indoorInterests'] = $indoorInterests;
                     $premiumMember->setIndoorInterests($indoorInterests);
                 } else {
-                    //$_SESSION['indoorInterests'] = "(No indoor interests selected)";
-
+                    $_SESSION['indoorInterests'] = "No indoor interests selected";
+                    $premiumMember->setIndoorInterests(array());
                 }
 
                 if (!empty($outdoorInterests)) {
                     //$_SESSION['outdoorInterests'] = implode(', ', $outdoorInterests);
+                    $_SESSION['outdoorInterests'] = $outdoorInterests;
                     $premiumMember->setOutDoorInterests($outdoorInterests);
                 } else {
-                    //$_SESSION['outdoorInterests'] = "(No outdoor interests selected)";
+                    $_SESSION['outdoorInterests'] = "No outdoor interests selected";
+                    $premiumMember->setOutDoorInterests(array());
                 }
 
                 $f3->reroute('/summary');
@@ -225,41 +192,24 @@
     });
 
     // defines "Summary" route
-    $f3->route('GET /summary', function($f3) {
-        /*
-        foreach($_POST['indoor-interests'] as $checked) {
-            echo "in door selected: $checked<br>";
-        }
+    $f3->route('GET /summary', function() {
+        //print_r($_SESSION);
 
-        echo "<br>";
+        $member = $_SESSION['membership'];
 
-        foreach($_POST['outdoor-interests'] as $checked) {
-            echo "out door selected: $checked<br>";
-        }
-        */
+        global $db;
+        $db->insertMember($member);
 
-        // note: hive variables CAN'T have "-"
-        //$f3->set('indoor', $_POST['indoor-interests']);
-        //$f3->set('outdoor', $_POST['outdoor-interests']);
-
-
+        //session_unset();
+        //session_destroy();
 
         $view = new Template();
         echo $view->render('views/summary.html');
     });
 
-// remove all session variables
-//session_unset();
-
-// destroy the session
-//session_destroy();
-
+    // defines "Contact" route
     $f3->route('GET /contact', function() {
-        // remove all session variables
-        session_unset();
-
-        // destroy the session
-        session_destroy();
+        //print_r($_SESSION);
 
         $view = new Template();
         echo $view->render('views/contact.html');
